3994d8b3d602f677652912eefa78f5d40a0f3577
hbrown10 <haydenbrown@comcast.net>
2023-04-27 23:30:46 -0400

    establishing postgres connection

---
+++ pgdb.js
@@ -0,0 +1,12 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+});
+
+client.connect();
+module.exports = client;
