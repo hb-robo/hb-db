fcb247546f406e9e1307db1aa4c6d9dfe2f33cfd
hbrown10 <haydenbrown@comcast.net>
2023-04-29 00:21:42 -0400

    establish pgdb connection

--- pgdb.js
+++ pgdb.js
@@ -1,12 +1,15 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  user: 'dbuser',
-  host: 'database.server.com',
-  database: 'mydb',
-  password: 'secretpassword',
-  port: 3211,
+  user: 'postgres',
+  host: 'containers-us-west-139.railway.app',
+  database: 'railway',
+  password: 'huykohQoskThIKwlZ97I',
+  port: 6371,
 });
 
-client.connect();
+client.connect()
+      .then(() => console.log('Connected to hb-db'))
+      .catch((err) => console.error('Connection error', err.stack));
+
 module.exports = client;
