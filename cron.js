bfcd305335e16f1ab2addb81258083ec7323cfd5
hbrown10 <haydenbrown@comcast.net>
2023-04-29 23:12:55 -0400

    finalize db sync cron job structure

--- cron.js
+++ cron.js
@@ -1,12 +1,18 @@
-// 
-const pgdb = require('./pgdb');
+// CRON TOOLS AND IMPORTS
 const cron = require('node-cron');
+import { syncGamesData } from './scripts/sync_games_data';
+import { syncMusicData } from './scripts/sync_music_data';
+import { syncGitHubData } from './scripts/sync_github_data';
+import { syncFitnessData } from './scripts/sync_fitness_data';
 
-function syncDB() {
-    syncGamesData();
-    syncMusicData();
-    syncBooksData();
-    syncFitnessData();
+
+async function syncDB() {
+    await Promise.allSettled([
+        syncGamesData(),
+        syncMusicData(),
+        syncGitHubData(),
+        syncFitnessData()
+    ]);
 }
 
 cron.schedule('0 2 * * *', syncDB);
