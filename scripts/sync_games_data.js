586e14d32b128c4e0948130528a0456c8d63e84a
hbrown10 <haydenbrown@comcast.net>
2023-05-01 00:29:23 -0400

    more logic for inserting new steam games to table

--- scripts/sync_games_data.js
+++ scripts/sync_games_data.js
@@ -9,6 +9,7 @@ const steam = require('steam-js-api');
 //==================== Steam API variables ====================
 const STEAM_API_KEY = 'B927A7E2DB6B29A3650B69AA54DB428C';
 const STEAM_USER_ID = '76561198038902641';
+
 const STEAM_GETOWNEDGAMES_URL = `` +
     `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/` +
     `?key=${STEAM_API_KEY}` +
@@ -19,12 +20,18 @@ const STEAM_GETOWNEDGAMES_URL = `` +
 const STEAM_GETGAMESCHEMA_URL = `` +
     `https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/` +
     `?key=${STEAM_API_KEY}`
-    `&appid=`; // will be appended later
+    `&appid=`; // appended later
 const STEAM_GETPLAYERACHIEVEMENTS_URL = `` +
     `http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/` +
     `?key=${STEAM_API_KEY}`
     `&steamid=${STEAM_USER_ID}` +
-    `&appid=`; // will be appended later
+    `&appid=`; // appended later
+const STEAM_GETGLOBALACHIEVEMENTRATES_URL = `` +
+    `https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v2/` +
+    `?gameid=` // appended later
+const STEAM_GETAPPDETAILS_URL = `` +
+    `http://store.steampowered.com/api/appdetails` +
+    `?appids=` // appended later
 
 /**
  * Hits the Steam API to update steam data in hb-db as few calls as posssible.
@@ -33,31 +40,22 @@ const STEAM_GETPLAYERACHIEVEMENTS_URL = `` +
  * 3) Adds all data for games not in database. 
  **/
 async function syncSteamData() {
-    // Grab list of games and versions in Steam library.
-    let game_list_raw = await pgdb.query(`
+    // Grab user's game library from Steam API.
+    const apiLibrary = await axios(STEAM_GETOWNEDGAMES_URL).then( result => {
+        console.log(result.data.games);
+        return result.data.games;
+    }).catch(console.error);
+    setTimeout(() => {}, 1000);
+
+    // Grab list of games and versions in DB to compare to.
+    let dbLibrary_raw = await pgdb.query(`
         SELECT appid, gameVersion
         FROM steam_owned_games;
     `);
-    const OWNED_GAMES = game_list_raw.rows;
+    var dbLibrary = dbLibrary_raw.rows;
+    const DB_APPID_LIST = dbLibrary.map(game => game.appid);
 
-    // Check for new game versions among current library and changes in achievement_count.
-    OWNED_GAMES.forEach(game => async function() {
-        let game_schema = await axios.get(`${STEAM_GETGAMESCHEMA_URL}${game.appid}`).catch(console.error);
-        if (game_schema.game.gameVersion != game.gameVersion) {
-            updateQuery = format(
-                `UPDATE steam_owned_games
-                SET gameVersion = %L,
-                    achievement_count = %L
-                WHERE appid = %L;`,
-                game_schema.game.gameVersion,
-                game_schema.game.achievements.length,
-                game.appid
-            )
-            await pgdb.query(updateQuery).catch(console.error);
-        }
-    });
-
-    // Grab maximum rtime_last_played value so we don't add/update data unnecessarily.
+    // Grab maximum rtime_last_played value, i.e. last time Steam was played in DB.
     let max_rtime_raw = await pgdb.query(`
         SELECT COALESCE(MAX(rtime_last_played),to_timestamp(0)) AS rtime_last_played
         FROM steam_owned_games;
@@ -65,91 +63,101 @@ async function syncSteamData() {
     const MAX_RTIME = new Date(max_rtime_raw.rows[0].rtime_last_played);
     const MAX_RTIME_UNIX = Math.floor(MAX_RTIME.getTime()/1000);
 
+    let gameInsertQueryHeader = 
+        `INSERT INTO steam_games (
+            service, platform, appid, game_title, developers, publishers, 
+            game_version, rtime_last_played, playtime, box_art_url,
+            has_achievements, unlocked_achievement_count, achievement_count)
+        VALUES `;
+    var gameRowsToInsert = [];
+    var insertArgs = [];
 
-    // Return JSON object of all games that either:
-    //    1) Have a new appid not yet in DB (new purchase), or
-    //    2) Have a rtime_last_played more recent than the max in DB (new playtime)
-    const data_to_sync = await axios(STEAM_GETOWNEDGAMES_URL).then( result => {
-        let games = result.data.games;
-        
-        let recentGames = games.filter(game => game.rtime_last_played > MAX_RTIME_UNIX && APPID_LIST.includes(game.appid));
-        let newGames = games.filter(game => !APPID_LIST.includes(game.appid));
+    // Handle each game based on what data needs to be updated or added.
+    for (let i = 0; i < apiLibrary.length; i++) {
+        let game = apiLibrary[i];
+        /** 
+         * Possible needed API calls per game:
+         *      GetSchemaForGame -> always true, need to check for new game version and achievement presence
+         *      GetGlobalAchievementPercentagesForApp -> only true if game has achievements
+         *      GetPlayerAchievements -> only if game is new or has been played since last sync, and has achievements
+         *      AppDetails -> only if game is new
+         **/ 
+        let isNew = !DB_APPID_LIST.includes(game.appid);
+        let wasPlayedRecently = DB_APPID_LIST.includes(game.appid) && game.rtime_last_played > MAX_RTIME_UNIX;
 
-        console.log(newGames);
-        return {recentGames, newGames};
-    }).catch(console.error);
+        // grabbing game schema
+        let gameSchema = await axios.get(`${STEAM_GETGAMESCHEMA_URL}${game.appid}`).catch(console.error);
+        setTimeout(() => {}, 1000);
 
-    // Update recently played games to check for progress made.
-    data_to_sync.recentGames.forEach(game => async function() {
-        let achievement_data = await axios(`${STEAM_GETPLAYERACHIEVEMENTS_URL}${game.appid}`).catch(console.error);
-        let updateQuery;
-        if (achievement_data.playerstats.hasOwnProperty('error')) { // game does not have achievement support
-            updateQuery = format(`
-                UPDATE steam_owned_games
-                SET rtime_last_played = %L,
-                WHERE appid = %L`,
-                game.rtime_last_played,
-                appid
-            );
-        } else {
-            unlocked_achievement_count = achievement_data.playerstats.achievements.filter(cheev => cheev.achieved === 1).length;
-            updateQuery = format(`
-                UPDATE steam_owned_games
-                SET rtime_last_played = %L,
-                    achievements_unlocked = %L
-                WHERE appid = %L`,
-                game.rtime_last_played,
-                unlocked_achievement_count,
-                appid
-            );
+        let hasAchievements = (Object.keys(gameSchema.game).length === 0);
+
+        // grabbing global achievement data
+        let globalAchievementData;
+        if(hasAchievements) {
+            globalAchievementData = await axios.get(`${STEAM_GETGLOBALACHIEVEMENTRATES_URL}${game.appid}`).catch(console.error);
+            setTimeout(() => {}, 1000);
         }
-        await pgdb.query(updateQuery).catch(console.error);
-    });
 
-    // Add all data for newly discovered games.
-    data_to_sync.newGames.forEach(game => async function() {
-        // grab game version
-        let game_schema = await axios.get(`${STEAM_GETGAMESCHEMA_URL}${game.appid}`).catch(console.error);
-        let game_version = game_schema.game.gameVersion
-        // grab achievement progress
-        let achievement_data = await axios(`${STEAM_GETPLAYERACHIEVEMENTS_URL}${game.appid}`).catch(console.error);
-        let has_achievements = achievement_data.playerstats.hasOwnProperty()
-        let achievement_list = achievement_data.playerstats.achievements
-        let achievement_count = achievement_list.length
-        let unlocked_achievement_count = achievement_list.filter(cheev => cheev.achieved === 1).length
-      
-        let insert_query;
-        if (has_achievements) {
-            insert_query = format(
-                `INSERT INTO 
-                steam_owned_games(
-                    appid, name, game_version, 
-                    rtime_last_played, playtime, box_art_url,
-                    has_achievements, achievement_count, unlocked_achievement_count)
-                VALUES (%L, %L, %L, %L, %L, %L)`,
-                row.appid, row.name, game_version,
-                row.rtime_last_played, row.playtime_forever,
-                `https://steamcdn-a.akamaihd.net/steam/apps/${row.appid}/library_600x900_2x.jpg`,
-                row.hasOwnProperty('has_community_visible_stats'),
-                achievement_count, unlocked_achievement_count
-            );
-        } else {
-            insert_query = format(
-                `INSERT INTO 
-                steam_owned_games(
-                    appid, name, game_version, 
-                    rtime_last_played, playtime, box_art_url,
-                    has_achievements)
-                VALUES (%L, %L, %L, %L, %L, %L)`,
-                row.appid, row.name, game_version,
-                row.rtime_last_played, row.playtime_forever,
-                `https://steamcdn-a.akamaihd.net/steam/apps/${row.appid}/library_600x900_2x.jpg`,
-                row.hasOwnProperty('has_community_visible_stats')
-            );
+        // grabbing player achievement data
+        let playerAchievementData;
+        if((isNew || wasPlayedRecently) && hasAchievements) {
+            playerAchievementData = await axios.get(`${STEAM_GETPLAYERACHIEVEMENTS_URL}${game.appid}`).catch(console.error);
+            setTimeout(() => {}, 1000);
         }
-        await pgdb.query(insert_query);
-    });
 
+        if(isNew) {
+            let appDetails;
+            appDetails = await axios.get(`${STEAM_GETAPPDETAILS_URL}${game.appid}`).catch(console.error);
+
+            /** service, platform, appid, game_title, developers, publishers, 
+            game_version, rtime_last_played, playtime, box_art_url,
+            has_achievements, unlocked_achievement_count, achievement_count) */
+            let comma = (gameRowsToInsert.length > 0) ? ',' : '';
+
+            let insertData;
+            let rowInsertArgs;
+            if(hasAchievements) {
+                insertData = `${comma}('Steam', 'PC', '${game.appid}', '${game.name}',
+                                '${appDetails[game.appid].data.developers}',
+                                '${appDetails[game.appid].data.publishers}',
+                                '${gameSchema.game.gameVersion}', %L, %L, 
+                                ${`https://steamcdn-a.akamaihd.net/steam/apps/${game.appid}/library_600x900_2x.jpg`},
+                                %L, %L, %L)`
+                let numCheevsUnlocked = playerAchievementData.playerstats.achievements.filter(cheev => cheev.achieved = 1).length;
+                let totalNumCheevs = playerAchievementData.playerstats.achievements.length;
+
+                rowInsertArgs = [game.rtime_last_played, game.played_forever,true, numCheevsUnlocked, totalNumCheevs];
+                insertArgs = insertArgs.concat(rowInsertArgs);
+            } else {
+                insertData = `${comma}('Steam', 'PC', ${game.appid}, ${game.name},
+                                ${appDetails[game.appid].data.developers},
+                                ${appDetails[game.appid].data.publishers},
+                                %L, %L, %L, 
+                                ${`https://steamcdn-a.akamaihd.net/steam/apps/${game.appid}/library_600x900_2x.jpg`},
+                                %L, %L, %L)`
+                rowInsertArgs = [null, game.rtime_last_played, game.played_forever,false, null, null];
+            }
+            gameRowsToInsert.push(insertData);
+            insertArgs = insertArgs.concat(rowInsertArgs);
+        }
+        else {
+            let dbData = (!isNew) ? dbLibrary.find(row => row.appid === game.appid) : null;
+            let hasNewVersion = dbData && gameSchema.game.gameVersion != game.gameVersion;
+
+            // update steam_games
+            // update steam_achievements
+        }
+    }
+
+    // Insert new game data if there is any
+    if (gameRowsToInsert.length > 0) {
+        gameInsertQueryString = `${gameInsertQueryHeader}${gameRowsToInsert.join()}`;
+        let gameInsertQuery = format(gameInsertQuery, ...insertArgs);
+        await pgdb.query(gameInsertQuery).catch(console.error);
+    }
+
+    // Insert new achievement data if there is any
+    
 }
 
 
@@ -157,9 +165,8 @@ async function syncSteamData() {
 //==================== RA API variables ====================
 
 
-async function syncRAData() {
+async function syncRAData() {}
 
-}
 
 async function syncGamesData() {
     await Promise.allSettled([
