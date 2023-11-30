8f8a2a12c4df8d9a0cb6c068d7c0801bb5c11444
hb-robo <haydenbrown@comcast.net>
2023-11-30 15:10:10 -0500

    feat: first draft of cytoscape home page

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,11 +1,26 @@
 // Import necessary modules
 import express, { Request, Response } from 'express';
+import path from 'path';
+import cytoscape from 'cytoscape';
+import fs from 'fs';
 
 // Create a new router
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+
+    fs.readFile(
+        path.join(__dirname, '../../public/json/homeGraph.json'), 'utf8', 
+        (err, data) => {
+            if (err) {
+                console.error(err);
+                return res.sendStatus(500);
+            }
+            const homeGraph = JSON.parse(data);
+            res.render('index', { homeGraph });
+        }
+    );   
+
 })
 
 export default router;
