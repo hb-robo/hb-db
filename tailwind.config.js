571ceda91d215ee820f418cf68808bc448bc535e
hbrown10 <haydenbrown@comcast.net>
2023-12-02 22:21:01 -0500

    chore: implement tailwindcss as CSS framework

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/*.ejs",
+    "./views/input/*.ejs",
+    "./views/output/*.ejs",
+    "./views/partials*.ejs",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
